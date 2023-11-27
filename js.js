8d9f6c6d80e88624d34064b96b69c75d1760ca81
Iris de Kruif <irisdekruif@hotmail.com>
2023-11-27 22:16:16 +0100

    index.html

--- js.js
+++ js.js
@@ -1,5 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
+    let gridItemAmount = 16
 
+    const grid = document.querySelector('.grid');
 
+    function createRows(gridItemAmount) {
+        const rowContainer = document.createElement('div');
+      
+        for (let i = 0; i < gridItemAmount; i++) {
+            const block = document.createElement('div');
+            rowContainer.appendChild(block);
+        }
+        grid.appendChild(rowContainer)
+    }
 
+    for (let i = 0; i < gridItemAmount; i++) {
+    createRows(gridItemAmount)
+    } 
 });
