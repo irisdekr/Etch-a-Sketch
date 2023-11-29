733897ba756efd6dd3689d910d4eda064be92bfd
Iris de Kruif <irisdekruif@hotmail.com>
2023-11-29 21:19:01 +0100

    js.js

--- js.js
+++ js.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
       
         for (let i = 0; i < gridItemAmount; i++) {
             const block = document.createElement('div');
+            block.classList.add("block");
             rowContainer.appendChild(block);
         }
         grid.appendChild(rowContainer)
@@ -17,4 +18,35 @@ document.addEventListener('DOMContentLoaded', function() {
     for (let i = 0; i < gridItemAmount; i++) {
     createRows(gridItemAmount)
     } 
+
+    const gridBlocks = document.querySelectorAll(".block");
+
+    gridBlocks.forEach(block => { 
+        block.addEventListener("mouseover", () => {
+            block.classList.add("hover-block");
+        });
+    });
 });
+
+// isDown=false;
+// secondsToHold=1
+// elem=document.querySelector('body');
+// elem.addEventListener('mousedown', function(event) { 
+// 	// simulating hold event
+//     if (isDown==false){
+// 		isDown=true;
+// 	    setTimeout(function() {
+// 			if (isDown==true){
+// 				console.log("Hold");
+// 			}
+// 		}, (secondsToHold*1000));
+// 	}
+// });
+// elem.addEventListener('mouseup', function(event) {
+// 	isDown=false;  
+// });
+
+// block.addEventListener("mouseover", evt => {
+//     if (evt.buttons === 1) {
+//       block.classList.add("hover-block");
+//     }
