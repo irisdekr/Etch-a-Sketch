1b4694a69f28cbaa66d0037341b1b6abfe12e98a
Iris de Kruif <irisdekruif@hotmail.com>
2023-12-02 20:31:35 +0100

    index.html

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     
+    //code for grid 
     let gridItemAmount = 16
 
     const grid = document.querySelector('.grid');
@@ -26,27 +27,17 @@ document.addEventListener('DOMContentLoaded', function() {
             block.classList.add("hover-block");
         });
     });
+
+        //code for button
+        document.querySelector('button').onclick = function() {popUp()};
+
+        //need to change 'string', now you can't turn in numbers between 16 and 100.
+        function popUp() {
+            let askGridSize = prompt("Choose a new gridsize by typing a number between 16 and 100:", "");
+                if (askGridSize == null || typeof(askGridSize) == 'string' || askGridSize <= 16 || askGridSize >= 100) {
+                    alert("This size is not available.");
+                } else {
+                let gridItemAmount = askGridSize //how to make this reach let gridItemAmount?
+                }
+          }
 });
-
-// isDown=false;
-// secondsToHold=1
-// elem=document.querySelector('body');
-// elem.addEventListener('mousedown', function(event) { 
-// 	// simulating hold event
-//     if (isDown==false){
-// 		isDown=true;
-// 	    setTimeout(function() {
-// 			if (isDown==true){
-// 				console.log("Hold");
-// 			}
-// 		}, (secondsToHold*1000));
-// 	}
-// });
-// elem.addEventListener('mouseup', function(event) {
-// 	isDown=false;  
-// });
-
-// block.addEventListener("mouseover", evt => {
-//     if (evt.buttons === 1) {
-//       block.classList.add("hover-block");
-//     }
